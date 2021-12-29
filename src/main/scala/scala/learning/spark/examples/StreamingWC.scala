6cea019e3dc63d1775d48344818a8bfe2eca866a
Chang Yong Lik <theeahlag@gmail.com>
2021-12-29 14:40:34 +0900

    first commit

---
+++ src/main/scala/scala/learning/spark/examples/StreamingWC.scala
@@ -0,0 +1,38 @@
+package scala.learning.spark.examples
+
+import java.util.logging.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object StreamingWC {
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .master("local[3]")
+      .appName("Streaming Word Count")
+      .config("spark.streaming.stopGracefullyOnShutdown", "true")
+      .getOrCreate()
+
+    val linesDF = spark.readStream
+                    .format("socket")
+                    .option("host", "localhost")
+                    .option("port", "9999")
+                    .load()
+
+//    linesDF.printSchema()
+
+    val wordsDF = linesDF.select(expr("explode(split(value,' ')) as word"))
+    val countsDF = wordsDF.groupBy("word").count()
+
+    val wordCountQuery = countsDF.writeStream
+                          .format("console")
+                          .outputMode("complete")
+                          .option("checkpointLocation", "chk-point-dir")
+                          .start()
+
+    logger.info("Listening to localhost:9999")
+    wordCountQuery.awaitTermination()
+  }
+
+}
